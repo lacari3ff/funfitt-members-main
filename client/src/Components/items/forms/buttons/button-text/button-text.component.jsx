892af70f7665636e0f022a75a53bf135d58721e6
lacari3ff <luke.rieff@gmail.com>
2019-08-01 10:08:51 +0200

    added build

--- client/src/Components/items/forms/buttons/button-text/button-text.component.jsx
+++ client/src/Components/items/forms/buttons/button-text/button-text.component.jsx
@@ -1,1 +1,11 @@
 import React, { Component } from "react";
+
+export default class ButtonText extends Component {
+  render() {
+    return (
+      <button className="button-text" onClick={this.props.onClick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
