e26cb148e418c4a4e63c773556e4f3e5eb8ac022
lacari3ff <luke.rieff@gmail.com>
2019-08-01 02:13:58 +0200

    Finishing tomorrow

--- client/src/Components/sidebar/sidebar-item/sidebar-item.component.jsx
+++ client/src/Components/sidebar/sidebar-item/sidebar-item.component.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import "./sidebar-item.styles.css";
+
 export default class SideBarItem extends Component {
   render() {
     return <div className="sidebar-item">{this.props.children}</div>;
--- client/src/Components/sidebar/sidebar.component.jsx
+++ client/src/Components/sidebar/sidebar.component.jsx
@@ -1,5 +1,16 @@
 import React, { COmponent } from "react";
 
+import InputDefault from "../items/forms/inputs/input-default/input-default.component";
+import SideBarItem from "./sidebar-item/sidebar-item.component";
+
 export default class SideBar extends COmponent {
-  render() {}
+  render() {
+    return (
+      <div className="sidebar">
+        <SideBarItem>
+          <InputBlank />
+        </SideBarItem>
+      </div>
+    );
+  }
 }
